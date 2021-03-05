2fde4321dedae6b41b7010b5c0366a0e233f17b5
Danilo <danilo@4cadia.com>
2021-03-05 08:24:53 -0300

    Vacina Campina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { VacinaComponent } from './vacina/vacina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    HomeComponent,
+    VacinaComponent
   ],
   imports: [
     BrowserModule,
